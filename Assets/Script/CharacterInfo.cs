4e48bc49fa363687d4aa8c5ea7a4812bf4f80286
PHNvieira015 <pedro.hvieira@al.infnet.edu.br>
2024-10-28 00:49:40 -0300

    test commit
    testing sprites

--- Assets/Script/CharacterInfo.cs
+++ Assets/Script/CharacterInfo.cs
@@ -5,5 +5,27 @@ using UnityEngine;
 public class CharacterInfo : MonoBehaviour
 {
     public OverlayTile standingOnTile;
+    
+    
+    //movement atributes
+    public float MoveSpeed;
+    public bool hasMoved;
 
+    //Attack atributes
+    public int playerNumber;
+    public float initiative;
+    public int attackRange;
+    private List<CharacterInfo> enemiesInRange = new List<CharacterInfo>();  //list of enemies in range
+    public int attackDamage;
+    public bool hasAttacked;
+
+    public GameObject weaponIcon;
+
+    //defense atributes
+    public int health;
+    public int defenseDamage;
+    public int armor;
+
+
+    
 }
