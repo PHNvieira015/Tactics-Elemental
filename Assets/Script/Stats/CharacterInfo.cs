7d7d78e1ed651bc1610dd77ae4673d0865725b73
PHNvieira015 <pedro.hvieira@al.infnet.edu.br>
2024-10-31 23:08:00 -0300

    Damage Method
    Starting Damage Method

--- Assets/Script/Stats/CharacterInfo.cs
+++ Assets/Script/Stats/CharacterInfo.cs
@@ -4,36 +4,61 @@ using UnityEngine;
 
 public class CharacterInfo : MonoBehaviour
 {
-    public OverlayTile standingOnTile;
+    // Base Information
+    public int CharacterID { get; private set; }
+    public string CharacterName { get; private set; }
+    public int CharacterLevel { get; private set; }
 
-    public bool selected;
-    private GameMaster gm;
+    // Resources
+    public int MaxBaseHealth { get; private set; }
+    public int CurrentHealth { get; private set; }
+    public int MaxMana { get; private set; }
+    public int CurrentMana { get; private set; }
 
-    //movement atributes
-    public float initiative;
-    public float MoveSpeed;
-    public bool hasMoved;
-    public int playerNumber;
-    private List<CharacterInfo> enemiesInRange = new List<CharacterInfo>();  //list of enemies in range
+    // Enums for various character attributes
+    public enum CharacterClass { Warrior, Mage, Archer }
+    public enum ElementType { Water, Fire, Wind, Earth, Thunder }
+    public enum AttackType { Crush, Slash, Pierce, Magic }
+    public enum ArmorType { Plate, Mail, Leather, Cloth }
+    public enum MovementType { Walking, Running, WaterWalk, LavaWalk, WaterSwimming, LavaSwimming, Levitate, Flying }
 
-    //Attack atributes
-    public int attackRange;
-    public int attackDamage;
-    public bool hasAttacked;
+    // Main Stats related to the character's abilities
+    [SerializeField] private int strength;  // Warrior main stat
+    [SerializeField] private int agility;   // Archer main stat
+    [SerializeField] private int intellect; // Mage main stat
 
-    public GameObject weaponIcon;
+    //derivative stats
+    [SerializeField] private int physicalPower;  //based on main stat and weapon type
+    [SerializeField] private int MagicalPower; //based on main stat and weapon type
+    [SerializeField] private float Accuracy; //based on main stat
 
-    //defense atributes
-    public int health;
-    public int Defense;
-    public int MagicDefense;
+    // Secondary Stats for enhancing character performance
+    [SerializeField] private int criticalChance; // Archer bonus
+    [SerializeField] private int criticalDamageMultiplier; // Archer bonus
+    [SerializeField] private int attackRange; // Range, melee is 1
 
-    //Magical Damage Types
-    public enum DamageType
-    {
-        Physical,
-        Fire,
-        Ice,
-        Poison
-    }
+    // Defense attributes to protect the character
+    [SerializeField] private ArmorType armorType; // Armor type
+    [SerializeField] private int armorValue;
+    [SerializeField] private int physicalDefense;
+    [SerializeField] private int magicalDefense;
+
+    // Movement attributes to define character mobility
+    [SerializeField] private int weight;
+    [SerializeField] private int speed; // Check if different from initiative
+    public float initiative; // Check if different from speed
+    public float movementRange;
+
+    // Description providing details about the character
+    public string characterDescription;
+
+    // Face Direction variable for character orientation
+    [SerializeField] private int faceDirection;
+
+    // Getters for main stats to encapsulate private fields
+    public int Strength => strength;
+    public int Agility => agility;
+    public int Intellect => intellect;
+
+    // You can add additional methods to handle character functionality here
 }
